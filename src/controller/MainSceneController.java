6214d941f40980b1188ceb86d968f121dc630d06
Pasquale Parillo <patparillo@gmail.com>
2022-01-07 13:46:53 -0800

    Initial commit JavaFX template

---
+++ src/controller/MainSceneController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class MainSceneController {
+
+    @FXML
+    private TextField tfTitle;
+
+    @FXML
+    void btnSubmit(ActionEvent event) {
+        Stage mainWindow = (Stage) tfTitle.getScene().getWindow();
+        String title = tfTitle.getText();
+        mainWindow.setTitle(title);
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,30 @@
+package main;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/view/MainScene.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Hello World!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
